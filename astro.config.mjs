320538f79d021f8c5795c66b3b17e7f6dbd50928
zcu <zachary.cromwell.uy@gmail.com>
2024-10-20 16:30:37 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  build: {
+    format: "directory",
+  },
   site: "https://www.zachuy.com",
   integrations: [tailwind()],
 });
